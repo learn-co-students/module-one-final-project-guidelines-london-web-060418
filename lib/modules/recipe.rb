cc12352c3e178182bbd591375016b0888aeb7fa2
github username <github email address>
2018-06-20 14:55:03 +0100

    Add methods to recipe class

--- lib/modules/recipe.rb
+++ lib/modules/recipe.rb
@@ -3,4 +3,40 @@ class Recipe < ActiveRecord::Base
   belongs_to :user
   has_many :recipe_ingredients
   has_many :ingredients, through: :recipe_ingredients
+
+  def initialize()
+    @template_recipe_id = get_random_meal_template.id
+    set_average_time
+  end
+
+  def get_random_meal_template
+    TemplateRecipe.all.sample
+  end
+
+  def set_average_time
+    self.avg_cooking_time = self.template_recipe.avg_cooking_time
+  end
+
+  def set_instructions
+    self.instruction = self.template_recipe.instructions
+  end
+
+  def get_random_ingredient(ingredient_type)
+
+  end
+
+  def get_ingredients
+    ingredient_requirement = template_recipe.ingredient_requirement
+
+    g = ingredient_requirement[:grains]
+    s = ingredient_requirement[:proteins]
+    v = ingredient_requirement[:vegetables]
+
+    self.ingredients = []
+
+    g.times do
+      
+
+  end
+
 end
