0eae969cc591890b793fae987d060e6ee20ed456
GCdM <gabrielcm.br@gmail.com>
2018-06-20 10:51:30 +0100

    Created Template table and changed Template model

--- lib/modules/template_recipe.rb
+++ lib/modules/template_recipe.rb
@@ -2,14 +2,15 @@
 #avg_cooking_time  =  int
 #ingredient_requirement = hash
 #instruction = hash
-class TempalteRecipe < ActiveRecord::Base
-  attr_accessor :type, :avg_cooking_time, :ingredient_requirement, :instructions
-
-  def initialize(hash)
-    @type = hash[:type]
-    @argument = hash[:avg_cooking_time]
-    @ingredient_requirement = hash[:ingredient_requirement]
-    @instructions = hash[:instructions]
-  end
+class TemplateRecipe < ActiveRecord::Base
 
 end
+
+# attr_accessor :type, :avg_cooking_time, :ingredient_requirement, :instructions
+
+# def initialize(hash)
+#   @type = hash[:type]
+#   @argument = hash[:avg_cooking_time]
+#   @ingredient_requirement = hash[:ingredient_requirement]
+#   @instructions = hash[:instructions]
+# end
