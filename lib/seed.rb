bea69ec68f4ed64c202f090a6678d275e6b0e6cd
GCdM <gabrielcm.br@gmail.com>
2018-06-20 13:48:39 +0100

    Created risotto TemplateRecipe seed

---
+++ lib/seed.rb
@@ -0,0 +1,25 @@
+#TEMPLATE RECIPES
+TemplateRecipe.create(
+  type: "Risotto",
+  avg_cooking_time: 60,
+  ingredient_requirement: "{
+    \"grains\":1,
+    \"proteins\":0,
+    \"vegetables\":3,
+    \"base_ingredients\":[
+      \"oil\",
+      \"stock\",
+      \"risotto rice\"
+    ]
+  }",
+  instructions: "{
+    \"1\":\"Chop your vegetables. Prepare stock and keep it to the side.\",
+    \"2\":\"In a big pan, cook all of your vegetables.\",
+    \"3\":\"Add the risotto rice, and stir well over a medium heat.\",
+    \"4\":\"When the rice starts to turn translucent, add a laddle of the stock and continue to stir.\",
+    \"5\":\"When most of the liquid has been absorbed, add another laddle of stock. Repeat this step until the rice is well cooked (approx. 20 min).\",
+    \"6\":\"Remove risotto from heat, and allow to cool.\",
+    \"7\":\"Serve onto plate(s) and add any accompanying garnish.\",
+    \"optional\":\"You can add some white wine to the pan with the risotto rice in step 3.\"
+  }"
+)
