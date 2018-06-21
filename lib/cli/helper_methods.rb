3af628faa73d2d949606f97e0ea2218c1cca671c
GCdM <gabrielcm.br@gmail.com>
2018-06-21 10:50:47 +0100

    CLI methods minus Recipe Book

--- lib/cli/helper_methods.rb
+++ lib/cli/helper_methods.rb
@@ -1,65 +1,105 @@
+#MAIN MENU
 def display_image
-  puts " \\     /"
-  puts " \\\\   //"
-  puts "  )\\-/("
-  puts "  /e e\\ "
-  puts " ( =Y= )"
-  puts " /`-!-'\\ "
-  puts " ____/ /___\\ \\ "
-  puts " \\   /    ```    ```~~\"--.,_"
+  puts "                     \\     /"
+  puts "                     \\\\   //"
+  puts "                      )\\-/("
+  puts "                      /e e\\ "
+  puts "                     ( =Y= )"
+  puts "                     /`-!-'\\ "
+  puts "        ___\\ \\      /       \\"
+  puts " \\   /    ```  --```~~\"--.,__\\"
   puts " `-._\\ /                       `~~\"--.,_"
   puts " ----->|                                `~~\"--.,_"
   puts " _.-'/ \\                                         ~~\"--.,_"
   puts "     \\_________________________,,,,....----\"\"\"\"~~~~````"
 end
 
-def main_prompt
+def main_menu
   display_image
-  puts "\nHow can I help you today:"
-  puts "      1 . Generate New Recipe"
-  puts "      2 . Browse Recipe Book"
-  puts "          3 . Help"
-  puts "          4 . Exit"
+  puts "\n  How can I help you:"
+  puts "\n             1 . Generate New Recipe"
+  puts "             2 . Browse Recipe Book"
+  puts "                   3 . Help"
+  puts "                   4 . Exit"
 end
 
+#1 - RECIPE GENERATOR
 def generate_recipe
+  recipe = Recipe.create
+  display_recipe(recipe)
+  puts "\nWould you like to add this recipe to your Recipe Book? (Y/N)"
+  loop do
+    input = gets.strip.downcase
 
+    if input == "y" || input == "yes"
+      # PersonalRecipe.create(recipe)
+      puts " - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
+      puts "\n  The recipe has been added to your Recipe Book!\n"
+      puts " - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
+      break
+    elsif input == "n" || input == "no"
+      puts "\nBon appetit!\n"
+      break
+    else
+      puts "I'm sorry, but I need a 'Yes'('Y') or a 'No'('N')"
+    end
+  end
 end
 
+def display_recipe(recipe)
+  instructions = JSON.parse(recipe.instructions)
+  ingredients = recipe.ingredients
+  puts "        - - - PLACEHOLDER - - -"
+  puts "\n  INGREDIENTS:\n"
+  ingredients.each do |ing|
+    puts "o #{ing.name}"
+  end
+  puts "\n  INSTRUCTIONS:\n"
+  instructions.each do |step, instruction|
+    puts "#{step} - #{instruction}"
+  end
+end
+
+#2 - RECIPE BOOK
 def recipe_book
 
 end
 
+#3 - HELP INFORMATION
 def help_info
-  puts "Help - Shows information about commands"
-  puts "1 . Generate New Recipe"
+  puts "\nHelp - Shows information about commands"
+  puts "\n1 . Generate New Recipe"
   puts "      Creates a new recipe using random ingredients."
   puts "      This recipe can then be saved to your Recipe Book."
-  puts "2 . Browse Recipe Book"
+  puts "\n2 . Browse Recipe Book"
   puts "      Browse your collection of saved recipes."
-  puts "4 . Exit"
+  puts "\n4 . Exit"
   puts "      Exit the program, discarding any unsaved recipes."
 end
 
-def exit_program
-  puts "Catch you later, doc!"
-end
-
+#RUN PROGRAM
 def run
-  puts "Welcome User! I'll be your recipe rabbit today!"
-  main_prompt
+  puts "\nWelcome User! I'll be your Recipe Rabbit for today!\n\n"
+  main_menu
   loop do
-    puts "\nPlease enter the number or the first word of a command:"
+    puts "\nEnter the number or the first word of a command:"
     input = gets.strip.downcase
     case input
-    when "1" || "generate" then generate_recipe
-    when "2" || "browse" then recipe_book
-    when "3" || "help" then help_info
-    when "4" || "exit" then
-      exit_program
+
+    when "1", "generate" then
+      generate_recipe
+      main_menu
+
+    when "2", "browse" then recipe_book
+
+    when "3", "help" then help_info
+
+    when "4", "exit" then
+      puts "\n Catch you later, doc!\n\n"
       break
+
     else
-      puts "Invalid command. Try again."
+      puts "Invalid command. Please try again."
     end
   end
 end
