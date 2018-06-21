206da9deedcc42e5a869189e05d023c4f8c8cd69
GCdM <gabrielcm.br@gmail.com>
2018-06-21 11:36:23 +0100

    Changes to schema and PersonalRecipe

--- db/migrate/20180620110049_create_personal_recipes_table.rb
+++ db/migrate/20180620110049_create_personal_recipes_table.rb
@@ -3,7 +3,6 @@ class CreatePersonalRecipesTable < ActiveRecord::Migration[5.0]
     create_table :personal_recipes do |t|
       t.integer :template_recipe_id
       t.string :name
-      t.integer :avg_cooking_time
       t.string :instructions
     end
   end
