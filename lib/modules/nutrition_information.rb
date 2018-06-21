53d4b4ee374cb39dcc84cdae9b902d35de857eac
github username <github email address>
2018-06-21 14:29:04 +0100

    populate nutrition fact table

--- lib/modules/nutrition_information.rb
+++ lib/modules/nutrition_information.rb
@@ -1,32 +1,40 @@
 require 'rest-client'
+require 'json'
 
 class NutritionFact < ActiveRecord::Base
-  # belongs_to :recipe
-  #
-  # def generate_query_string
-  #   recipe.ingredients.map do |ingredient|
-  #     ingredient.name
-  #   end.join(",")
-  # end
-  #
-  # def generate_url
-  #   url = "https://trackapi.nutritionix.com/v2/natural/nutrients/"
-  #
-  #   RestClient.post(url, query_string ,headers={"Content-Type" => "application/json",
-  #     "x-app-id" => "e9b0130f",
-  #     ":x-app-key" => "4c0a16bd2267c3640d1917a1d3438fca"},
-  #
-  #     )
-  #
-  #   url = "https://trackapi.nutritionix.com/v2/natural/nutrients/#{generate_query_string}&appId=e9b0130f&appKey=4c0a16bd2267c3640d1917a1d3438fca"
-  # end
-  #
-  # def get_data
-  #
-  #  response = HTTParty.get(generate_url)
-  #  json = JSON.parse(@response.body)
-  #
-  # end
-  #
+  belongs_to :recipe
+
+  def generate_query_string
+    ingredient_string = recipe.ingredients.map do |ingredient|
+      ingredient.name
+    end.join(", ")
+
+    # query_string = "#{ingredient_string}"
+    ingredient_string
+  end
+
+
+  def get_data
+    url = "https://trackapi.nutritionix.com/v2/natural/nutrients/"
+    response = RestClient.post(url, {query: generate_query_string}.to_json, {
+      "Content-Type" => "application/json",
+      "x-app-id" => "e9b0130f",
+      "x-app-key" => "4c0a16bd2267c3640d1917a1d3438fca"
+    })
+
+    nutrion_hash = JSON.parse(response)
+  end
+
+  def get_macro(macro)
+    get_data["foods"].map do |food|
+      food[macro]
+    end.reduce {|a,b| a + b}
+  end
+
+  def set_total_macros
+    self.protein = get_macro("nf_protein")
+    self.carbs = get_macro("nf_total_carbohydrate")
+    self.fat = get_macro("nf_total_fat")
+  end
 
 end
