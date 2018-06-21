179112312315d3304d567b7e9b4222f92bf7c333
GCdM <gabrielcm.br@gmail.com>
2018-06-21 11:11:18 +0100

    Add methods to personal recipe class + small edits to schema

--- db/migrate/20180620110049_create_personal_recipes_table.rb
+++ db/migrate/20180620110049_create_personal_recipes_table.rb
@@ -1,10 +1,10 @@
 class CreatePersonalRecipesTable < ActiveRecord::Migration[5.0]
   def change
     create_table :personal_recipes do |t|
+      t.integer :template_recipe_id
+      t.string :name
       t.integer :avg_cooking_time
-      t.string :ingredients
       t.string :instructions
-      t.string :nutritional_info
     end
   end
 end
--- lib/modules/personal_recipe.rb
+++ lib/modules/personal_recipe.rb
@@ -2,4 +2,15 @@ class PersonalRecipe < ActiveRecord::Base
   belongs_to :user
   has_many :recipe_ingredients
   has_many :ingredients, through: :recipe_ingredients
+
+  after_create :copy_attributes
+
+  def copy_attributes
+    self.template_recipe = Recipe.last.template_recipe
+    self.instructions = Recipe.last.instructions
+    Recipe.last.ingredients.each do |ing|
+      RecipeIngredient.create(recipe: self, ingredient: ing)
+    end
+    self.nutritional_info = Recipe.last.nutritional_info
+  end
 end
