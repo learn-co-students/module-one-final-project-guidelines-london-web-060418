2c005f5ae5bbff8d40d3f29ac524f566157258a0
GCdM <gabrielcm.br@gmail.com>
2018-06-21 14:26:52 +0100

    User authentication and sign up

--- lib/cli/helper_methods.rb
+++ lib/cli/helper_methods.rb
@@ -1,3 +1,10 @@
+#LOGIN MENU
+def start_menu
+  puts "\n                1. Login"
+  puts "               2. Sign up"
+  puts "                3. Exit\n"
+end
+
 #MAIN MENU
 def display_image
   puts "                     \\     /"
@@ -17,10 +24,10 @@ end
 def main_menu
   display_image
   puts "\n  How can I help you:"
-  puts "\n             1 . Generate New Recipe"
-  puts "             2 . Browse Recipe Book"
-  puts "                   3 . Help"
-  puts "                   4 . Exit"
+  puts "\n             1. Generate New Recipe"
+  puts "             2. Browse Recipe Book"
+  puts "                   3. Help"
+  puts "                   4. Exit"
 end
 
 #1 - RECIPE GENERATOR
@@ -32,10 +39,10 @@ def generate_recipe
     input = gets.strip.downcase
 
     if input == "y" || input == "yes"
-      # PersonalRecipe.create(recipe)
-      puts " - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
+      PersonalRecipe.create(template_recipe_id: recipe.template_recipe_id)
+      puts "   - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
       puts "\n  The recipe has been added to your Recipe Book!\n"
-      puts " - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
+      puts "   - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
       break
     elsif input == "n" || input == "no"
       puts "\nBon appetit!\n"
@@ -62,7 +69,9 @@ end
 
 #2 - RECIPE BOOK
 def recipe_book
+  book = PersonalRecipe.all.select do |p_r|
 
+  end
 end
 
 #3 - HELP INFORMATION
@@ -79,27 +88,84 @@ end
 
 #RUN PROGRAM
 def run
-  puts "\nWelcome User! I'll be your Recipe Rabbit for today!\n\n"
-  main_menu
+  puts "\n/\\/\\/\\/\\/\\ - VEGAN GENERATOR - /\\/\\/\\/\\/\\"
+  user = nil
   loop do
-    puts "\nEnter the number or the first word of a command:"
+    start_menu
+    puts "\nEnter a command or number:"
     input = gets.strip.downcase
     case input
 
-    when "1", "generate" then
-      generate_recipe
-      main_menu
+    when "1", "login"
+      puts "\n      L O G I N\n\n"
+      print " Username: "
+      username = gets.strip
 
-    when "2", "browse" then recipe_book
+      if User.find_by(username: username)
+        print " Password: "
+        password = gets.strip
 
-    when "3", "help" then help_info
+        if User.find_by(username: username).password == password
+          user = User.find_by(username: username)
+          break
+        else
+          puts "   - - - - - - - - - - - - - - - - - -"
+          puts "   - X - X - Wrong password - X - X -"
+          puts "   - - - - - - - - - - - - - - - - - -"
+        end
 
-    when "4", "exit" then
-      puts "\n Catch you later, doc!\n\n"
+      else
+        puts "   - - - - - - - - - - - - - - - - - - - - - - - -"
+        puts "   - X - X - That account doesn't exist - X - X -"
+        puts "   - - - - - - - - - - - - - - - - - - - - - - - -"
+      end
+
+    when "2", "sign up"
+      puts "\n      S I G N   U P\n\n"
+      print " Username: "
+      username = gets.strip
+      if User.find_by(username: username)
+        puts "\n - X - X - This account already exists - X - X -\n"
+      else
+        print " Password: "
+        password = gets.strip
+        User.create(username: username, password: password)
+        puts "\n   - - - - - - - - - - - - - - - -"
+        puts "   New account succesfully created"
+        puts "   - - - - - - - - - - - - - - - -\n"
+      end
+
+    when "3", "exit"
+      puts "      Goodbye!\n"
       break
-
     else
       puts "Invalid command. Please try again."
+    end
+  end
+
+  if user != nil
+    puts "\nWelcome User! I'll be your Recipe Rabbit for today!\n\n"
+    main_menu
+    loop do
+      puts "\nEnter the number or the first word of a command:"
+      input = gets.strip.downcase
+      case input
+
+      when "1", "generate" then
+        generate_recipe
+        main_menu
+
+      when "2", "browse" then recipe_book
+        
+      when "3", "help" then help_info
+
+      when "4", "exit" then
+        puts "\n Catch you later, doc!\n\n"
+        break
+
+      else
+        puts "Invalid command. Please try again."
+      end
     end
   end
 end
